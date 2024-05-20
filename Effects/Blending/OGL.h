f6f30bff5f0a7c1f8d9455744aa19b08193ebcd7
Ashish Shelke <exampleemail@example>
2024-05-20 19:55:04 +0530

    Deleted for new update

--- Effects/Blending/OGL.h
+++
@@ -1,5 +0,0 @@
-#pragma once
-
-#define MYICON 101 // Unique integer value define macro
-#define MY_SMILEY 102
- 
--- Effects/Marathi_Font_Rendering/Text2Glyph_Converter/Font2Glyph_Converter.py
+++
@@ -1,8 +0,0 @@
-from Krutidev2Unicode import Unicode_to_KrutiDev
-
-#from Krutidev2Unicode import do_convert
-
-#print(KrutidevToUnicode.convert_to_unicode(';gk\u00a1 is vkidk ;wfudksM'))
-
-print(Unicode_to_KrutiDev("AataM ivaXvaa%makoM dovaoM । yaoNaoM vaagya&oM tÜYaavaoM । "))
-
